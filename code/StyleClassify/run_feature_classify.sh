b28b88f170e0c5bfbc6bd5255cd41b9d6ba72c67
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-17 04:22:10 -0500

    Change output file names of gypsum

--- code/StyleClassify/run_feature_classify.sh
+++ code/StyleClassify/run_feature_classify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=stylecls
-#SBATCH --output=logs/hh_%j.txt  # output file
-#SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
+#SBATCH --output=logs/sc_%j.txt  # output file
+#SBATCH -e logs/sc_%j.err        # File to which STDERR will be written
 #SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --gres=gpu:3
 #SBATCH --mem=42G
--- code/StyleTransfer/glove/constructEmbeddingDump.sh
+++ code/StyleTransfer/glove/constructEmbeddingDump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=stylecls
-#SBATCH --output=logs/hh_%j.txt  # output file
-#SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
+#SBATCH --job-name=styletran
+#SBATCH --output=logs/st_%j.txt  # output file
+#SBATCH -e logs/st_%j.err        # File to which STDERR will be written
 #SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --gres=gpu:3
 #SBATCH --mem=42G
