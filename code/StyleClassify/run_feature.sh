3551e045a8a5ec43d00d96e0cdfcea61bb998f0b
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-16 22:49:15 -0500

    Increase memory req for gypsum

--- code/StyleClassify/run_feature.sh
+++ code/StyleClassify/run_feature.sh
@@ -4,6 +4,7 @@
 #SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
 #SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --gres=gpu:3
+#SBATCH --mem=42G
 #
 #SBATCH --ntasks=1
 
