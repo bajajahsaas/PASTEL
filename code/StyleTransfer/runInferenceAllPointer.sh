f0b3fe1c0775ff2986b977f2dd2441844e822b8d
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-12-09 14:32:14 -0500

    Run in 0_10 configuration

--- code/StyleTransfer/runInferenceAllPointer.sh
+++ code/StyleTransfer/runInferenceAllPointer.sh
@@ -26,7 +26,7 @@ testName=test
 #    echo "Done for $sty"
 #done
 
-modelName=simpleModelGlove2BothPretrainedPointer20_20
+modelName=simpleModelGlove2BothPretrainedPointer0_10
 
 for sty in STYLED #Politics ethnic gender Country edu TOD
 do
