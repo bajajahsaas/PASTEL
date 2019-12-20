716cd3fd52d9bdf8f9394870f79b362f75ceca9d
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-12-19 23:22:14 -0500

    Add -decAttn in Args

--- code/StyleTransfer/runInferenceAll.sh
+++ code/StyleTransfer/runInferenceAll.sh
@@ -31,7 +31,7 @@ modelName=simpleModelGlove2BothPretrained
 for sty in STYLED #Politics ethnic gender Country edu TOD
 do
     echo "Running Inference For $sty"
-    CUDA_VISIBLE_DEVICES=1 python -u main.py -mode=inference -method=beam -emb_size=300 -hidden_size=384 -modelName=tmp/${modelName}_${sty}_${styNo[$sty]}.ckpt -problem=$sty | tee logsinfer/Inference_${modelName}_${sty}_${styNo[$sty]}
+    CUDA_VISIBLE_DEVICES=1 python -u main.py -decAttn mode=inference -method=beam -emb_size=300 -hidden_size=384 -modelName=tmp/${modelName}_${sty}_${styNo[$sty]}.ckpt -problem=$sty | tee logsinfer/Inference_${modelName}_${sty}_${styNo[$sty]}
     echo "Computing Metrics For $sty"
     python -u computeNLGEvalMetrics.py tmp/${modelName}_${sty}_${styNo[$sty]}.ckpt.test.beam.output data/${testName}.${sty}.tgt | tee logsinfer/NLGEval_${modelName}_${sty}_${styNo[$sty]}
     echo "Done for $sty"
